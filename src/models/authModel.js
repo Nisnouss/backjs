dacab0f216253bd7044b7cf7e4df58d68c6f515a
Nisrine <nisnouss8348@gmail.com>
2026-02-26 09:26:39 +0100

    création des models pour authentification

---
+++ src/models/authModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+        email:{
+            type: String,
+            required: [true, "Veuillez ajouter un mail"],
+            unique: true,
+            match: [
+                /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                'Veuillez ajouter un mail valide'
+            ]
+        },
+        password:{
+            type: String,
+            required: [true, 'Veuillez ajouter un mot de passe'],
+            minlenght: 8,
+            select: false // Ne pas retourner le mot de passe par défaut
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+// Hasher le mot de passe avant la sauvegarde
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')){
+        return next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
