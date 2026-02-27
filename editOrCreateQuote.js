093451d83cfd57c52ea371c9352013175df6973e
Nisrine <nisnouss8348@gmail.com>
2026-02-27 16:44:51 +0100

    Projet finis

--- editOrCreateQuote.js
+++ editOrCreateQuote.js
@@ -1,18 +1,72 @@
+const url2 = new URL(window.location.href)
+const id = url2.searchParams.get("id")
+console.log(id)
 // Afficher une quote
 async function getQuote() {
-    const url = 'http://localhost:3000/api/quote/all'
+    const url = `http://localhost:3000/api/quote/${id}`
     const req = await fetch(url,{
         method: 'GET',
     })
-    const datas = await req.json()
+    const data = await req.json()
     const divQuote = document.querySelector('div');
-    datas.forEach(data => {
-        divQuote.insertAdjacentHTML('afterbegin', `<p>${data.quote}</p>`);
-        divQuote.insertAdjacentHTML('afterbegin', `<p>${data.author}</p>`);
-    });
+    divQuote.insertAdjacentHTML('afterbegin', `<p>${data.author}</p>`);
+    divQuote.insertAdjacentHTML('afterbegin', `<p>${data.quote}</p>`);
+    divQuote.insertAdjacentHTML('afterbegin', `<p>${data._id}</p>`);
+    const updateQuote = document.querySelector('input[id = "updQuote"]')
+    const updateAuthor = document.querySelector('input[id = "updAuthor"]')
+    updateQuote.value = data.quote
+    updateAuthor.value = data.author
     // console.log(await datas.json())
 }
 getQuote();
+
+// Modifier
+const buttonUdpate = document.querySelector('button[id = "update"]')
+
+buttonUdpate.addEventListener('click', async(e) => {
+    e.preventDefault()
+    const updateQuote = document.querySelector('input[id = "updQuote"]')
+    const updateAuthor = document.querySelector('input[id = "updAuthor"]')
+    const updateQuoteVal = updateQuote.value
+    const updateAuthorVal = updateAuthor.value
+
+    const obj = {
+        quote: updateQuoteVal,
+        author: updateAuthorVal
+    }
+    const url = `http://localhost:3000/api/quote/${id}`
+
+    const req = await fetch(url, {
+        method: "PUT",
+        body: JSON.stringify(obj),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YTAxMGUyZjVmNDRhY2EyOWU1MGE1YiIsImlhdCI6MTc3MjA5OTcwMSwiZXhwIjoxNzc0NjkxNzAxfQ.KVrg1Tul2CJTzAVgkhD2gcgl7yM9HO__SccjnMOdyCk`
+        }
+    })
+
+    const data = await req.json()
+    console.log(data)
+    localStorage.setItem("obj", JSON.stringify(obj))
+})
+
+// Supprimer
+const buttonDelete = document.querySelector('button[id = "delete"')
+
+buttonDelete.addEventListener('click', async(e) => {
+    e.preventDefault()
+    
+    const url = `http://localhost:3000/api/quote/${id}`
+
+    const req = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YTAxMGUyZjVmNDRhY2EyOWU1MGE1YiIsImlhdCI6MTc3MjA5OTcwMSwiZXhwIjoxNzc0NjkxNzAxfQ.KVrg1Tul2CJTzAVgkhD2gcgl7yM9HO__SccjnMOdyCk`
+        }
+    })
+
+    const data = await req.json()
+})
 
 // Créer
 const buttonCreate = document.querySelector('button[id = "create"]')
@@ -39,53 +93,8 @@ buttonCreate.addEventListener('click', async(e) => {
         },
     })
 
-    const data = await req.json()
-    console.log(data)
-})
-// Modifier
-const buttonUdpate = document.querySelector('button[id = "update"]')
+    const datas = await req.json()
+    //console.log(datas)
 
-buttonUdpate.addEventListener('click', async(e) => {
-    e.preventDefault()
-    const updateQuote = document.querySelector('input[id = "updQuote"]')
-    const updateAuthor = document.querySelector('input[id = "updAuthor"]')
-    const updateQuoteVal = updateQuote.value
-    const updateAuthorVal = updateAuthor.value
-
-    const obj = {
-        quote: updateQuoteVal,
-        author: updateAuthorVal
-    }
-    const url = `http://localhost:3000/api/quote/${id}`
-
-
-    const req = await fetch(url, {
-        method: "PUT",
-        body: JSON.stringify(obj),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YTAxMGUyZjVmNDRhY2EyOWU1MGE1YiIsImlhdCI6MTc3MjA5OTcwMSwiZXhwIjoxNzc0NjkxNzAxfQ.KVrg1Tul2CJTzAVgkhD2gcgl7yM9HO__SccjnMOdyCk`
-        }
-    })
-
-    const data = await req.json()
-    console.log(data)
-})
-
-// Supprimer
-const buttonDelete = document.querySelector('button[id = "delete"')
-
-buttonDelete.addEventListener('click', async(e) => {
-    e.preventDefault()
-    
-    const url = `http://localhost:3000/api/quote/${id}`
-
-    const req = await fetch(url, {
-        method: 'DELETE',
-        headers: {
-            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YTAxMGUyZjVmNDRhY2EyOWU1MGE1YiIsImlhdCI6MTc3MjA5OTcwMSwiZXhwIjoxNzc0NjkxNzAxfQ.KVrg1Tul2CJTzAVgkhD2gcgl7yM9HO__SccjnMOdyCk`
-        }
-    })
-
-    const data = await req.json()
+    localStorage.setItem("obj", JSON.stringify(obj))
 })
--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+
 // Afficher une quote
-async function getQuote() {
-    const url = 'http://localhost:3000/api/quote'
+async function getAiQuote() {
+    const url = 'http://localhost:3000/api/quote/aiQuote'
     const req = await fetch(url,{
         method: 'GET',
     })
     const data = await req.json()
-    const divQuote = document.querySelector('div');
-    divQuote.insertAdjacentHTML('afterbegin', `<p>${data.quote}</p>`);
-    divQuote.insertAdjacentHTML('beforeend', `<p>${data.author}</p>`);
+    const divQuoteAi = document.querySelector('div[id = "aiquo"]');
+    divQuoteAi.insertAdjacentHTML('afterbegin', `<p>${data.author}</p>`);
+    divQuoteAi.insertAdjacentHTML('beforeend', `<p>${data.quote}</p>`);
     // console.log(await datas.json())
 }
-getQuote();
+getAiQuote();
+
+// Afficher une quote
+async function getAllQuote() {
+    const url = 'http://localhost:3000/api/quote/all'
+    const req = await fetch(url,{
+        method: 'GET',
+    })
+    const datas = await req.json()
+    const divQuoteAll = document.querySelector('div[id = "allquo"]');
+    datas.forEach(data => {
+        divQuoteAll.insertAdjacentHTML('beforeend', `<p>${data.quote}</p>`);
+        divQuoteAll.insertAdjacentHTML('beforeend', `<p>${data.author}</p>`);
+        divQuoteAll.insertAdjacentHTML('beforeend', `<a href="editOrCreateQuote.html?id=${data._id}"> Afficher plus </a>`);
+    });
+
+    // console.log(await datas.json())
+}
+getAllQuote();
+
 
 // Authentification
 const button = document.querySelector('button');
@@ -22,13 +42,13 @@ button.addEventListener('click', async(e) => {
     const valuePwd = inputPwd.value
     const url = 'http://localhost:3000/api/auth/login'
 
-    const obj = {
+    const loginInfo = {
         email: valueMail,
         password: valuePwd
     }
     const req = await fetch(url,{
         method: 'POST',
-        body: JSON.stringify(obj),
+        body: JSON.stringify(loginInfo),
         headers: {
             'Content-Type': 'application/json',
         },
@@ -36,7 +56,7 @@ button.addEventListener('click', async(e) => {
 
     const data = await req.json()
 
-    localStorage.setItem('data', json.stringify(data))
+    localStorage.setItem('loginInfo', json.stringify(loginInfo))
 })
 
 // Création d'un compte
@@ -49,15 +69,16 @@ buttonRegister.addEventListener('click', async(e) => {
     const valuePwdRegister = inputPwdRegister.value
     const url = 'http://localhost:3000/api/auth/register'
 
-    const obj = {
+    const registerInfo = {
         email: valueMailRegister,
         password: valuePwdRegister
     }
     const datas = await fetch(url,{
         method: 'POST',
-        body: JSON.stringify(obj),
+        body: JSON.stringify(registerInfo),
         headers: {
             'Content-Type': 'application/json'
         },
     })
+    localStorage.setItem('registerInfo', JSON.stringify(registerInfo))
 })
